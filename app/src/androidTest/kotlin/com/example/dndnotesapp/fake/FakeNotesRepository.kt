dc83e2b4b277d2e1087a79948c2a93d3937563c7
Dreadyyyy <90562473+Dreadyyyy@users.noreply.github.com>
2024-03-07 23:13:32 +0100

    More tests
    Finished writing fake repository for unit tests. Wrote tests for fake repository for unit tests

--- app/src/androidTest/kotlin/com/example/dndnotesapp/fake/FakeNotesRepository.kt
+++ app/src/androidTest/kotlin/com/example/dndnotesapp/fake/FakeNotesRepository.kt
@@ -1,30 +1,47 @@
 package com.example.dndnotesapp.fake
 
+import android.util.Log
 import com.example.dndnotesapp.data.Note
 import com.example.dndnotesapp.data.NotesRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
+private const val TAG: String = "FakeNotesRepository"
 class FakeNotesRepository : NotesRepository {
-    val note1: Note = Note(id = 0, headline = "Headline1", text = "Text1")
-    val note2: Note = Note(id = 1, headline = "Headline2", text = "Text2")
+    val note0: Note = Note(id = 0, headline = "Headline0", text = "Text0")
+    val note1: Note = Note(id = 1, headline = "Headline1", text = "Text1")
+    private val notesList: MutableList<Note> = mutableListOf(
+        note0, note1
+    )
     override suspend fun insertNote(note: Note) {
+        notesList.add(note.copy(id = notesList.size))
     }
 
     override suspend fun updateNote(note: Note) {
+        try {
+            notesList[note.id] = note
+        } catch (exception: IndexOutOfBoundsException) {
+            Log.e(TAG, exception.message ?: "Unknown error")
+        }
     }
 
     override suspend fun deleteNote(note: Note) {
+        try {
+            notesList.removeAt(note.id)
+            (note.id..notesList.lastIndex).forEach {index: Int->
+                notesList[index] = notesList[index].copy(id = index)
+            }
+        } catch (exception: IndexOutOfBoundsException) {
+            Log.e(TAG, exception.message ?: "Unknown error")
+        }
     }
 
     override fun getAllNotes(): Flow<List<Note>> =
         flow {
-            emit(
-                listOf(note1, note2)
-            )
+            while (true) emit(notesList)
         }
 
     override fun getNote(id: Int): Flow<Note?> = flow {
-        emit(note1)
+        while (true) emit(notesList.getOrNull(id))
     }
 }
--- app/src/androidTest/kotlin/com/example/dndnotesapp/fake/FakeNotesRepositoryTest.kt
+++ app/src/androidTest/kotlin/com/example/dndnotesapp/fake/FakeNotesRepositoryTest.kt
@@ -3,14 +3,67 @@ package com.example.dndnotesapp.fake
 import com.example.dndnotesapp.data.Note
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class FakeNotesRepositoryTest {
     private val fakeNotesRepository: FakeNotesRepository = FakeNotesRepository()
+
     @Test
-    fun fakeNotesRepository_getAllNotesCalled_flowWithListOfTwoNotesReturned() = runTest{
+    fun fakeNotesRepository_getAllNotesCalled_flowWithListOfTwoNotesReturned() = runTest {
         val notesList: List<Note> = fakeNotesRepository.getAllNotes().first()
         assertEquals(2, notesList.size)
+    }
+
+    @Test
+    fun fakeNotesRepository_getNoteWithIndexOneCalled_rightNoteReturned() = runTest {
+        val note: Note? = fakeNotesRepository.getNote(0).first()
+        assertEquals(fakeNotesRepository.note0, note)
+    }
+
+    @Test
+    fun fakeNotesRepository_insertNote2_noteInsertedSuccessfully() = runTest {
+        insertOneNote()
+        val note: Note? = fakeNotesRepository.getNote(2).first()
+        assertEquals(note2, note)
+    }
+
+    @Test
+    fun fakeNotesRepository_insertedTwoNotesAndOneUpdated_fakeRepositoryReturnsCorrectList() =
+        runTest {
+            insertTwoNotes()
+            fakeNotesRepository.updateNote(updatedNote2)
+            val expectedList: List<Note> = listOf(
+                fakeNotesRepository.note0, fakeNotesRepository.note1,
+                updatedNote2, note3
+            )
+            val noteList: List<Note> = fakeNotesRepository.getAllNotes().first()
+            assertEquals(expectedList, noteList)
+        }
+
+    @Test
+    fun fakeNotesRepository_twoNotesAddedOneDeleted_fakeRepositoryReturnsCorrectIndexedList() = runTest {
+            insertTwoNotes()
+            fakeNotesRepository.deleteNote(note2)
+            val expectedList: List<Note> = listOf(
+                fakeNotesRepository.note0, fakeNotesRepository.note1,
+                reIndexedNote3
+            )
+            val noteList: List<Note> = fakeNotesRepository.getAllNotes().first()
+            assertEquals(expectedList, noteList)
+        }
+
+    private suspend fun insertOneNote() = fakeNotesRepository.insertNote(note2)
+    private suspend fun insertTwoNotes() {
+        fakeNotesRepository.insertNote(note2)
+        fakeNotesRepository.insertNote(note3)
+    }
+
+    private companion object {
+        private val note2: Note = Note(id = 2, headline = "headline2", text = "text2")
+        private val note3: Note = Note(id = 3, headline = "headline3", text = "text3")
+        private val updatedNote2 = Note(id = 2, headline = "Headline2", text = "Text2")
+        private val reIndexedNote3: Note = Note(id = 2, headline = "headline3", text = "text3")
     }
 }
